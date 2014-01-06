4b90173c9d7d7e6985986d38af50eea3d49dc5c7
Kyle Lahnakoski <klahnakoski@mozilla.com>
2014-01-05 21:44:28 -0500

    fix minimum variance==0 problem

--- dzAlerts/daemons/alert_exception.py
+++ dzAlerts/daemons/alert_exception.py
@@ -364,13 +364,13 @@ def alert_exception(settings, db):
 def single_ttest(point, stats, min_variance=0):
     n1 = stats.count
     m1 = stats.mean
-    v1 = stats.variance
+    v1 = max(stats.variance, 1.0/12.0)  # VARIANCE OF STANDARD UNIFORM DISTRIBUTION
 
     if n1 < 2:
         return {"confidence": 0, "diff": 0}
 
     try:
-        tt = (point - m1) / sqrt(max(min_variance, v1))    #WE WILL IGNORE UNUSUALLY GOOD TIMINGS
+        tt = (point - m1) / sqrt(v1)
         t_distribution = scipy.stats.distributions.t(n1 - 1)
         confidence = t_distribution.cdf(tt)
         return {"confidence": confidence, "diff": tt}
--- dzAlerts/daemons/alert_sustained.py
+++ dzAlerts/daemons/alert_sustained.py
@@ -14,7 +14,6 @@ import numpy
 import scipy
 from scipy import stats
 from dzAlerts import util
-from dzAlerts.daemons.alert_exception import single_ttest
 from dzAlerts.util import struct
 from dzAlerts.util.cnv import CNV
 from dzAlerts.util.maths import Math
@@ -407,16 +406,16 @@ def welchs_ttest(stats1, stats2):
     """
     n1 = stats1.count
     m1 = stats1.mean
-    v1 = stats1.variance
+    v1 = max(stats1.variance, 1.0/12.0)
 
     n2 = stats2.count
     m2 = stats2.mean
-    v2 = stats2.variance
+    v2 = max(stats2.variance, 1.0/12.0)
 
     if n1 < 2 or n2 < 2:
         return {"confidence": 0, "diff": 0}
 
-    vpooled = max(v1 / n1 + v2 / n2, 1.0 / 12.0)
+    vpooled = v1 / n1 + v2 / n2
     # 1/12 == STD OF STANDARD UNIFORM DISTRIBUTION
     # We assume test replicates (xi) are actually rounded results from
     # actual measurements somewhere in the range of (xi - 0.5, xi + 0.5),
@@ -426,7 +425,6 @@ def welchs_ttest(stats1, stats2):
     df_numerator = vpooled ** 2
     df_denominator = ((v1 / n1) ** 2) / (n1 - 1) + ((v2 / n2) ** 2) / (n2 - 1)
     df = df_numerator / df_denominator
-
 
     # abs(x - 0.5)*2 IS AN ATTEMPT TO GIVE HIGH NUMBERS TO EITHER TAIL OF THE cdf
     return {"confidence": abs(stats.t(df).cdf(tt) - 0.5) * 2, "diff": tt}
