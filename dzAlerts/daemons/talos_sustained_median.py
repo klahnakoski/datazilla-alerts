02a9ab7e8be5e15b83ce5fe85982d6cfe6890463
Kyle Lahnakoski <klahnakoski@mozilla.com>
2014-06-03 14:49:16 -0400

    use ttest to identify most likely regression candidate (not tested)

--- dzAlerts/daemons/talos_sustained_median.py
+++ dzAlerts/daemons/talos_sustained_median.py
@@ -19,6 +19,7 @@ from dzAlerts.util.queries.es_query import ESQuery
 from dzAlerts.util.env import startup
 from dzAlerts.util.queries.db_query import DBQuery, esfilter2sqlwhere
 from dzAlerts.daemons.util.median_test import median_test
+from dzAlerts.daemons.util.welchs_ttest import welchs_ttest
 from dzAlerts.util.cnv import CNV
 from dzAlerts.util.queries import windows
 from dzAlerts.util.queries.query import Query
@@ -64,7 +65,7 @@ def alert_sustained_median(settings, qb, alerts_db):
     query = settings.query
 
     def is_bad(r):
-        if settings.param.sustained_median.trigger < r.result.confidence:
+        if settings.param.sustained_median.trigger < r.ttest_result.confidence:
             test_param = set_default(
                 settings.param.suite[literal_field(r.Talos.Test.suite)],
                 settings.param.test[literal_field(r.Talos.Test.name)],
@@ -232,10 +233,17 @@ def alert_sustained_median(settings, qb, alerts_db):
                         "aggregate": windows.Stats(middle=0.60),
                         "range": {"min": 0, "max": settings.param.sustained_median.window_size}
                     }, {
+                        "name": "ttest_result",
+                        "value": lambda r, i, rows:  welchs_ttest(
+                            rows[-settings.param.sustained_median.window_size + i:i:].value,
+                            rows[ i:settings.param.sustained_median.window_size + i:].value
+                        ),
+                        "sort": "push_date"
+                    }, {
                         "name": "result",
                         "value": lambda r, i, rows: median_test(
                             rows[-settings.param.sustained_median.window_size + i:i:].value,
-                            rows[i:settings.param.sustained_median.window_size + i:].value,
+                            rows[ i:settings.param.sustained_median.window_size + i:].value,
                             interpolate=False
                         ),
                         "sort": "push_date"
@@ -266,7 +274,7 @@ def alert_sustained_median(settings, qb, alerts_db):
             #PICK THE BEST SCORE FOR EACH is_diff==True REGION
             for g2, data in Q.groupby(stats, "is_diff", contiguous=True):
                 if g2.is_diff:
-                    best = Q.sort(data, ["result.confidence", "diff"]).last()
+                    best = Q.sort(data, ["ttest_result.confidence", "diff"]).last()
                     best["pass"] = True
 
             all_touched.update(Q.select(test_results, ["test_run_id", "Talos.Test"]))
