1ce44e0a042e752201b4fdf7db06b4a490e4fdef
Kyle Lahnakoski <klahnakoski@mozilla.com>
2014-06-27 13:43:42 -0400

    missed another item

--- resources/scripts/talos_alerts.bat
+++ resources/scripts/talos_alerts.bat
@@ -3,6 +3,6 @@ REM RUN FROM MAIN DIRECTORY
 SET PYTHONPATH=.
 
 CALL pypy dzAlerts\daemons\sustained_median.py  --settings_file "./talos_settings.json"
-REM CALL pypy dzAlerts\daemons\talos_alert_revision.py  --settings_file "./talos_settings.json"
+CALL pypy dzAlerts\daemons\talos_alert_revision.py  --settings_file "./talos_settings.json"
 REM CALL pypy dzAlerts\daemons\alert.py  --settings_file "./talos_settings.json"
 REM CALL pypy dzAlerts\daemons\email_send.py  --settings_file "./talos_settings.json"
