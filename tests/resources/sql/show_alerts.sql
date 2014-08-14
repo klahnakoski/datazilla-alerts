30b621fce37d089a1c2c17787cdfdc5180c60753
Kyle Lahnakoski <klahnakoski@mozilla.com>
2014-08-14 08:39:21 -0400

    fix warning logging

--- tests/resources/sql/show_alerts.sql
+++ tests/resources/sql/show_alerts.sql
@@ -1,18 +1,21 @@
 use alerts;
 
 SELECT
-	json.string(a.details, "Device") device,
-	json.string(a.details, "Branch") branch,
-	json.string(json.json(a.details, "Test"), "name") test_name,
-	json.string(json.json(a.details, "Test"), "suite") test_suite,	
+--  	json.string(a.details, "Device") device,
+--  	json.string(a.details, "Branch") branch,
+ 	json.string(json.json(a.details, "Test"), "name") test_name,
+  	json.string(json.json(a.details, "Test"), "suite") test_suite,
+	json.number(a.details, "diff_percent") diff_percent,	
 	a.*	
 FROM 
 	alerts a 
 WHERE
-	a.reason like 'talos%' and
-	details like '%71a1dcb2697d%'
+ 	a.reason='talos_alert_revision' and
+	last_sent is null and
+	create_time > date_add(now(), interval -30 day) and
+	status <> 'obsolete'
 ORDER BY
-	create_time DESC
-LIMIT 
-	100
+-- 	json.string(json.json(a.details, "Test"), "name"),
+	a.create_time desc,
+	a.revision
 ;
--- tests/resources/sql/show_mail.sql
+++ tests/resources/sql/show_mail.sql
@@ -3,7 +3,7 @@ select
 from 
 	mail.content 
 where
-	subject = '[ALERT][Mozilla-Inbound] json-parse-financial regressed by 5.2% in kraken'
+	date_sent is null
 order by 
 	coalesce(date_sent, now()) desc 
 limit 
