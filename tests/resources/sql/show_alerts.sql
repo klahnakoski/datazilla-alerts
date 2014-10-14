d0aa7d45d469f9481042a6f0191c7d616a04a0fe
Kyle Lahnakoski <klahnakoski@mozilla.com>
2014-10-14 09:04:59 -0400

    throw error if trying to convert None to url

--- tests/resources/sql/show_alerts.sql
+++ tests/resources/sql/show_alerts.sql
@@ -5,10 +5,13 @@ SELECT
 FROM
 	alerts a
 WHERE
---  	a.reason like 'b2g%' and
-	last_sent is null and
-	push_date > date_add(now(), interval -30 day) and
-	status <> 'obsolete'
+-- 	a.revision ='aed50d3edf33' and
+	a.test = '1-customize-enter.error.TART' and 
+--  	a.branch = 'Mozilla-Aurora' and
+--   	a.reason like '%rev%' and
+-- 	last_sent is null and
+	push_date > date_add(now(), interval -30 day) #and
+-- 	status <> 'obsolete'
 ORDER BY
 -- 	json.string(json.json(a.details, "Test"), "name"),
 	a.push_date desc,
